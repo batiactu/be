8834bdc42826811cb3d79eccc910bf2ac96d36b0
Alexis Algoud <alexis@capinfopro.com>
2013-06-18 18:11:28 +0200

    Extraction id_edito a corriger

--- js/actu.js
+++ js/actu.js
@@ -36,3 +36,22 @@ function initActu() {
 	});
 	
 }
+function showArticle(url_article) {
+	
+	
+	var reg=new RegExp("$[0-9]+(.php)","g");
+	id_edito = reg.exec(url_article); 
+//	 alert(id_edito);
+	/* populate annonce */
+	$.mobile.changePage('#article');
+
+	$('#annonce').getItem({
+		url:'http://www.batiactu.com/cap_batiactu/scripts/get-edito.php'
+		,data: {
+			id_edito : id_edito
+			,jsonp:'jsonp'
+		}
+	});
+	
+	
+}
