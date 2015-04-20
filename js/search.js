16f6cb2c8f2dcd476871184c30d0cfd7c8f7ef72
dpouchon <david.pouchon@batiactugroupe.com>
2015-04-20 10:46:15 +0200

    gestion seo

--- js/search.js
+++ js/search.js
@@ -197,6 +197,14 @@ function validate_cac(type) {
 	window[tabName] = newTabType;
 }
 
+function resetTotalCurrent(type) {
+	var tabName = 'current_' + type;
+
+	var newTabType = new Array;
+	window[tabName] = newTabType;
+}
+
+
 function resetTmp_cac(type) {
 	var tabName = 'current_' + type;
 	var tabNameTmp = 'current_' + type + '_tmp';
@@ -1157,7 +1165,7 @@ function go_url_recherche(){
 
 	$.mobile.changePage( newurl);
 
-	return true; 
+	return true;
 }
 
 
